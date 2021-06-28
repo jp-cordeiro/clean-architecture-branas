a3355311862d1fee82426ebb6ee76970ce7253d9
JP <jpc.araujo@hotmail.com>
2021-06-28 00:16:32 -0300

    test: add EnrollStudent test initial config

---
+++ tests/EnrollStudent.test.ts
@@ -0,0 +1,7 @@
+import EnrollStudent from '@/EnrollStudent'
+
+describe('EnrollStudent', () => {
+  test('should not enroll without valid student name', () => {
+    const enrollStudent = new EnrollStudent()
+  })
+})
