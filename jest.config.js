2fff69bbbba50d82a1bfdfad9246597d2381e274
JP <jpc.araujo@hotmail.com>
2021-06-28 00:06:29 -0300

    chore: initial config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: ['<rooDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
